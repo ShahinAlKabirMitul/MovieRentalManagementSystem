3b1b489676113d437d936f29e829d35d5ad376c8
mitulbd <mitul.cse@gmail.com>
2017-08-19 12:18:45 +0600

    First Migration

---
+++ MovieRentalManagementSystem/Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using MovieRentalManagementSystem.Models;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace MovieRentalManagementSystem.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            var movie = new Movie() { Name = "Boos Baby" };
+            return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return  Content("Edit ID: "+id);
+        }
+    }
+}
---
+++ MovieRentalManagementSystem/Models/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieRentalManagementSystem.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
