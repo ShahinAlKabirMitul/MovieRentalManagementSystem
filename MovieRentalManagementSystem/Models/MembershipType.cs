ba3f257ce5aa8d0845453bcb97fc6eef48c0b478
mitulbd <mitul.cse@gmail.com>
2017-08-19 12:46:27 +0600

    Add Seed Data To Membeshiptype

---
+++ MovieRentalManagementSystem/Models/MembershipType.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieRentalManagementSystem.Models
+{
+    public class MembershipType
+    {
+        public byte Id { get; set; }
+        public short SignUpFee { get; set; }
+        public byte DurationInMonth { get; set; }
+        public byte DiscountRate { get; set; }
+
+    }
+}
