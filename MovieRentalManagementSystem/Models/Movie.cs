33c837462ee789efa9b07ffbb4834d8226cb5cae
mitulbd <mitul.cse@gmail.com>
2017-08-20 00:01:03 +0600

    Update Movie Database Table

--- MovieRentalManagementSystem/Models/Movie.cs
+++ MovieRentalManagementSystem/Models/Movie.cs
@@ -1,15 +1,31 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieRentalManagementSystem.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
+
         public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        [Display(Name = "Added Date")]
         public DateTime AddedDate { get; set; }
+
+        [Display(Name = "Number Of Sotck")]
+        [Required]
         public int Stock { get; set; }
+
         public Genre Genre { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
         public int GenreId { get; set; }
 
     }
