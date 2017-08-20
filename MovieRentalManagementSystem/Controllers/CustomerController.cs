d30fb9dacdbd86843e245fa2163d5782134b993f
mitulbd <mitul.cse@gmail.com>
2017-08-20 21:19:51 +0600

    Add anti-forgery token

--- MovieRentalManagementSystem/Controllers/CustomerController.cs
+++ MovieRentalManagementSystem/Controllers/CustomerController.cs
@@ -30,6 +30,7 @@ namespace MovieRentalManagementSystem.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
             if (!ModelState.IsValid)
