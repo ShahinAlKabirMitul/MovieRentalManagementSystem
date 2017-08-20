d6df115c54dd541e8ac69962083e2e582e796bea
mitulbd <mitul.cse@gmail.com>
2017-08-20 07:34:30 +0600

    Custom Validation

--- MovieRentalManagementSystem/Models/Customer.cs
+++ MovieRentalManagementSystem/Models/Customer.cs
@@ -19,6 +19,7 @@ namespace MovieRentalManagementSystem.Models
         public byte MembershipTypeId { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfMember]
         public DateTime? BirthDay { get; set; }
 
     }
---
+++ MovieRentalManagementSystem/Models/Min18YearsIfMember.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieRentalManagementSystem.Models
+{
+    public class Min18YearsIfMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+            if (customer.MembershipTypeId == 1)
+            {
+                return ValidationResult.Success;
+            }
+            if (customer.BirthDay == null)
+            {
+                return new ValidationResult("Bith day is required");
+            }
+            var age = DateTime.Today.Year - customer.BirthDay.Value.Year;
+            return (age >= 18)
+                ? ValidationResult.Success
+                : new ValidationResult("Customer should be at least 18 years to go on a membership");
+        }
+    }
+}
