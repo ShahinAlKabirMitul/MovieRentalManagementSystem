e0c5dfd14f448e747f567b3ef8c02842ce3f7e3d
mitulbd <mitul.cse@gmail.com>
2017-08-21 07:06:18 +0600

    Add Customer Dto

---
+++ MovieRentalManagementSystem/Dtos/CustomerDto.cs
@@ -0,0 +1,22 @@
+﻿using MovieRentalManagementSystem.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieRentalManagementSystem.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        [Min18YearsIfMember]
+        public DateTime? BirthDay { get; set; }
+    }
+}
